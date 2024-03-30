59cee8f7e0dd3e6ffb826ca6806fa0af3dac26a7
Masaya1582 <masa8y88@gmail.com>
2024-03-30 12:45:56 +0900

    Delete RxDatasources

--- Xib_Playground/Sources/Views/CollectionViewCells/HomeCollectionViewCell.swift
+++ Xib_Playground/Sources/Views/CollectionViewCells/HomeCollectionViewCell.swift
@@ -8,7 +8,6 @@
 import UIKit
 import RxSwift
 import RxCocoa
-import RxDataSources
 
 final class HomeCollectionViewCell: UICollectionViewCell {
     // MARK: - Properties
--- Xib_Playground/Sources/Views/TableViewCells/HomeTableViewCell.swift
+++ Xib_Playground/Sources/Views/TableViewCells/HomeTableViewCell.swift
@@ -8,7 +8,6 @@
 import UIKit
 import RxSwift
 import RxCocoa
-import RxDataSources
 
 final class HomeTableViewCell: UITableViewCell {
     // MARK: - Properties
