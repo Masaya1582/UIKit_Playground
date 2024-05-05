5c5eb5e4407b8032a15583f71721fc0f886fb89a
Masaya Nakakuki <masa8y88@gmail.com>
2024-05-06 08:49:11 +0900

    Rx & Load

--- Xib_Playground/Sources/ViewControllers/HomeViewController.swift
+++ Xib_Playground/Sources/ViewControllers/HomeViewController.swift
@@ -14,16 +14,9 @@ final class HomeViewController: UIViewController {
     typealias Dependency = HomeViewModelType
 
     // MARK: - Properties
-//    @IBOutlet private weak var tableView: UITableView! {
-//        didSet {
-//            tableView.registerCell(HomeTableViewCell.self)
-//        }
-//    }
-//    @IBOutlet private weak var collectionView: UICollectionView! {
-//        didSet {
-//            collectionView.registerCell(HomeCollectionViewCell.self)
-//        }
-//    }
+    @IBOutlet private weak var loadButton: UIButton!
+    @IBOutlet private weak var loadingView: UIActivityIndicatorView!
+
     private let disposeBag = DisposeBag()
     private let viewModel: Dependency
 
@@ -49,53 +42,18 @@ final class HomeViewController: UIViewController {
 // MARK: - Bind
 private extension HomeViewController {
     func bind(to viewModel: Dependency) {
-//        <#Button#>.rx.tap.asSignal()
-//            .emit(onNext: { [weak self] in
-//                <#Actions#>
-//            })
-//            .disposed(by: disposeBag)
-//
-//        <#TextField#>.rx.text.orEmpty
-//            .bind(to: <#ViewModel#>.inputs.<#Property#>)
-//            .disposed(by: disposeBag)
-//
-//        viewModel.outputs.<#Property#>
-//            .drive { [weak self] <#Property#> in
-//                <#Actions#>
-//            }
-//            .disposed(by: disposeBag)
-//
-//        viewModel.outputs.<#Property#>
-//            .drive(<#tableView or collectionView#>.rx.items) { <#tableView or collectionView#>, row, element in
-//                let indexPath = IndexPath(row: row, section: 0)
-//               let cell = <#tableView or collectionView#>.dequeueReusableCell(<#TableViewCell or CollectionViewCell#>.self, for: indexPath)
-//                cell.configure(with: element)
-//                return cell
-//            }
-//            .disposed(by: disposeBag)
-//
-//        viewModel.outputs.listItem
-//            .drive(<#tableView or collectionView#>.rx.items) { <#tableView or collectionView#>, row, element in
-//                switch element {
-//                case .<#enum Item1#>:
-//                    guard let cell = <#tableView or collectionView#>.dequeueReusableCell(withIdentifier: "<#Identifier#>", for: [0, row]) as? <#TableView or CollectionView#> else {
-//                        return UITableViewCell()
-//                    }
-//                    return cell
-//                case .<#enum Item2#>(let <#property#>):
-//                    guard let cell = <#tableView or collectionView#>.dequeueReusableCell(withIdentifier: "<#Identifier#>", for: [0, row]) as? <#TableView or CollectionView#> else {
-//                        return UITableViewCell()
-//                    }
-//                    cell.configure(with: element)
-//                    return cell
-//                case .<#enum Item3#>:
-//                    guard let cell = <#tableView or collectionView#>.dequeueReusableCell(withIdentifier: "<#Identifier#>", for: [0, row]) as? <#TableView or CollectionView#> else {
-//                        return UITableViewCell()
-//                    }
-//                    return cell
-//                }
-//            }
-//            .disposed(by: disposeBag)
+        loadButton.rx.tap.asSignal()
+            .emit(onNext: { [weak self] in
+                self?.viewModel.inputs.load.accept(())
+            })
+            .disposed(by: disposeBag)
+
+        viewModel.outputs.isLoading
+            .drive { [weak self] isLoading in
+                self?.loadingView.startAnimating()
+                self?.loadingView.isHidden = !isLoading
+            }
+            .disposed(by: disposeBag)
     }
 }
 
