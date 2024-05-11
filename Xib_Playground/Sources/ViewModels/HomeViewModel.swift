93001a353182dc6cacd5517c27ebf182c22f6268
Masaya1582 <masa8y88@gmail.com>
2024-05-12 08:45:33 +0900

    Rx & Timer Lesson

--- Xib_Playground/Sources/ViewModels/HomeViewModel.swift
+++ Xib_Playground/Sources/ViewModels/HomeViewModel.swift
@@ -10,10 +10,14 @@ import RxCocoa
 import Action
 
 protocol HomeViewModelInputs: AnyObject {
+    var manageCount: PublishRelay<Void> { get }
+    var resetCount: PublishRelay<Void> { get }
 }
 
 protocol HomeViewModelOutputs: AnyObject {
-    // var items: Driver<[HomeViewModel.ListItem]> { get }
+    var count: Driver<Double> { get }
+    var shouldHideResetButton: Driver<Bool> { get }
+    var isTimerRunning: Driver<Bool> { get }
 }
 
 protocol HomeViewModelType: AnyObject {
@@ -27,23 +31,53 @@ final class HomeViewModel: HomeViewModelType, HomeViewModelInputs, HomeViewModel
     var outputs: HomeViewModelOutputs { return self }
 
     // MARK: - Input Sources
+    let manageCount = PublishRelay<Void>()
+    let resetCount = PublishRelay<Void>()
     // MARK: - Output Sources
-    // let items: Driver<[ListItem]>
+    let count: Driver<Double>
+    let shouldHideResetButton: Driver<Bool>
+    let isTimerRunning: Driver<Bool>
 
     // MARK: - Properties
+    private let _count = BehaviorRelay<Double>(value: 0.0)
+    private let _shouldHideResetButton = BehaviorRelay<Bool>(value: true)
+    private let _isTimerRunning = BehaviorRelay<Bool>(value: false)
+    private var timer: Disposable?
     private let disposeBag = DisposeBag()
 
     // MARK: - Initialize
     init() {
+        self.count = _count.asDriver(onErrorDriveWith: .empty())
+        self.shouldHideResetButton = _shouldHideResetButton.asDriver(onErrorDriveWith: .empty())
+        self.isTimerRunning = _isTimerRunning.asDriver(onErrorDriveWith: .empty())
+
+        manageCount
+            .subscribe(onNext: { [weak self] _ in
+                guard let strongSelf = self else { return }
+                if strongSelf._isTimerRunning.value == true {
+                    strongSelf.timer?.dispose()
+                    strongSelf._isTimerRunning.accept(false)
+                } else {
+                    strongSelf.timer = Observable<Int>.interval(.milliseconds(100), scheduler: MainScheduler.instance)
+                        .subscribe(onNext: { _ in
+                            let newCount = strongSelf._count.value + 0.1
+                            let formattedCount = Double(round(10 * newCount) / 10)
+                            strongSelf._count.accept(formattedCount)
+                            strongSelf._shouldHideResetButton.accept(false)
+                        })
+                    strongSelf._isTimerRunning.accept(true)
+                }
+            })
+            .disposed(by: disposeBag)
+
+        resetCount
+            .subscribe(onNext: { [weak self] _ in
+                self?._count.accept(0.0)
+                self?._shouldHideResetButton.accept(true)
+                self?.timer?.dispose()
+                self?._isTimerRunning.accept(false)
+            })
+            .disposed(by: disposeBag)
     }
 
 }
-
-// MARK: - Item
-//extension HomeViewModel {
-//    enum ListItem {
-//        case header
-//        case shop(Content)
-//        case footer
-//    }
-//}
