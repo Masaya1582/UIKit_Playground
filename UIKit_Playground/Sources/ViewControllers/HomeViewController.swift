e5f26dbb41ce5b31444aef0168b2a7f487f108c0
Masaya1582 <masa8y88@gmail.com>
2024-09-22 17:59:46 +0900

    調整

--- UIKit_Playground/Sources/ViewControllers/HomeViewController.swift
+++ UIKit_Playground/Sources/ViewControllers/HomeViewController.swift
@@ -11,12 +11,12 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     // MARK: - Dependency
-
     typealias Dependency = HomeViewModelType
 
     // MARK: - Properties
     @IBOutlet private weak var accrodionButton: UIButton!
     @IBOutlet private weak var accrodionView: UIView!
+    @IBOutlet private weak var accordionViewHeightConstraint: NSLayoutConstraint!
 
     private lazy var viewModel: HomeViewModelType = { fatalError("Use (dependency: ) at initialize controller") }()
     private let disposeBag = DisposeBag()
@@ -41,15 +41,27 @@ final class HomeViewController: UIViewController {
 
 // MARK: - Bind
 private extension HomeViewController {
-    func bind(to _: Dependency) {
+    func bind(to: Dependency) {
         accrodionButton.rx.tap.asSignal()
             .emit(to: viewModel.inputs.switchAccrodion)
             .disposed(by: disposeBag)
 
         viewModel.outputs.isAccordionViewHidden
-            .drive { [weak self] isAccordionViewHidden in
-                self?.accrodionView.isHidden = isAccordionViewHidden
-            }
+            .drive(onNext: { [weak self] isAccordionViewHidden in
+                guard let self = self else { return }
+                if !isAccordionViewHidden {
+                    self.accrodionView.isHidden = false
+                }
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.accordionViewHeightConstraint.constant = isAccordionViewHidden ? 0 : 160
+                    self.view.layoutIfNeeded()
+                    self.accrodionView.alpha = isAccordionViewHidden ? 0.0 : 1.0
+                }, completion: { _ in
+                    if isAccordionViewHidden {
+                        self.accrodionView.isHidden = true
+                    }
+                })
+            })
             .disposed(by: disposeBag)
     }
 }
