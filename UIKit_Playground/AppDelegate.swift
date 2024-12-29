ea290236bf98f5b9f674ecc9761a52f6bb24ed8b
Masaya1582 <masa8y88@gmail.com>
2024-12-29 11:37:40 +0900

    Fix Lint

--- UIKit_Playground/AppDelegate.swift
+++ UIKit_Playground/AppDelegate.swift
@@ -10,12 +10,10 @@ import UIKit
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
