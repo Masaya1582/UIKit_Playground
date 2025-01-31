787550c9da3827c51e213573fac600d5780ba604
Masaya Nakakuki <masa8y88@gmail.com>
2025-01-31 13:20:36 +0900

    SharedInstance

---
+++ UIKit_Playground/Sources/Models/Sharedinstance.swift
@@ -0,0 +1,13 @@
+//
+//  Sharedinstance.swift
+//  UIKit_Playground
+//
+//  Created by MasayaNakakuki on 2025/01/31.
+//
+
+import Foundation
+
+final class Sharedinstance {
+    static let shared = Sharedinstance()
+    private init() {}
+}
